df09de59364a4a9a85ab03dd56429b94c475237e
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-31 09:49:49 -0300

    chore: setup logs

--- src/Report.Generator/Program.cs
+++ src/Report.Generator/Program.cs
@@ -1,18 +1,22 @@
 ﻿using Report.Generator.Infrastructure.Contexts;
 
+using Serilog;
+
 namespace Report.Generator;
 
 internal class Program
 {
     static async Task Main()
     {
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         DateTime referenceDate = UI.GetReferenceDateUserInput();
         string providerName = "AWS";
 
-        Console.WriteLine($"Data de referência: {referenceDate:dd/MM/yyyy}");
-        Console.WriteLine($"Cloud Provider: {providerName}");
+        Log.Information($"Iniciando processamento de relatório em formato .csv referente à data: {referenceDate:dd/MM/yyyy} em {providerName}");
 
         ReportGeneratorContext context = new(providerName);
         await context.ExecuteAsync(new(referenceDate));
+
+        Log.Information("Finalizado processamento de relatório");
     }
 }
