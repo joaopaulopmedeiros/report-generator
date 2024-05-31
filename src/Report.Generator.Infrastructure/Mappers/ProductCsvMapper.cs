e813239fcc8f4596fa1e7e425a57ce5b4ecbe88f
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-31 13:40:17 -0300

    feat: custom map

---
+++ src/Report.Generator.Infrastructure/Mappers/ProductCsvMapper.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration;
+
+using Report.Generator.Domain.Entities;
+
+namespace Report.Generator.Infrastructure.Mappers;
+
+public class ProductCsvMapper : ClassMap<Product>
+{
+    public ProductCsvMapper()
+    {
+        Map(m => m.Id).Index(0).Name("id");
+        Map(m => m.Title).Index(1).Name("titulo");
+        Map(m => m.Price).Index(2).Name("preço");
+        Map(m => m.CreatedAt).Index(3).Name("data de criação").TypeConverterOption.Format("dd/MM/yyyy");
+    }
+}
