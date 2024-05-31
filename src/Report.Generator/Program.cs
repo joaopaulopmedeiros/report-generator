68a46bd2b7e7b34612a88dc58ae925b8c59f4675
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-05-31 08:43:27 -0300

    feat: get user input params

--- src/Report.Generator/Program.cs
+++ src/Report.Generator/Program.cs
@@ -2,8 +2,29 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Por favor, especifique o número total de produtos do relatório:");
+        string? totalProdutosInput = Console.ReadLine();
+        int totalProdutos;
+
+        while (!int.TryParse(totalProdutosInput, out totalProdutos) || totalProdutos <= 0)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro positivo para o número total de produtos:");
+            totalProdutosInput = Console.ReadLine();
+        }
+
+        Console.WriteLine("Por favor, especifique a data de referência (formato: dd/mm/aaaa):");
+        string? dataReferenciaInput = Console.ReadLine();
+        DateTime dataReferencia;
+
+        while (!DateTime.TryParseExact(dataReferenciaInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dataReferencia))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira uma data no formato dd/mm/aaaa:");
+            dataReferenciaInput = Console.ReadLine();
+        }
+
+        Console.WriteLine($"\nNúmero total de produtos: {totalProdutos}");
+        Console.WriteLine($"Data de referência: {dataReferencia:dd/MM/yyyy}");
     }
 }
